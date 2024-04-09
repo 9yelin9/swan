faee2c0ba978c10a946980d20595ac4e3fe65656
9yelin9 <9yelin9@gmail.com>
2024-04-09 15:42:23 +0900

    Use ASE package to get spacegroup

--- swann.py
+++ swann.py
@@ -2,11 +2,11 @@
 
 swann_path = '/home/9yelin9/swann'
 band_path = 'swann_band'
-lat_columns = ['i', 'j', 'k', 'p', 'q', 't_real', 't_imag']
+lat_cols = ['i', 'j', 'k', 'p', 'q', 't_real', 't_imag']
 lat_dtypes = {'i':'i', 'j':'i', 'k':'i', 'p':'i', 'q':'i', 't_real':'d', 't_imag':'d'}
 
 import os
-num_threads = 1
+num_threads = 16
 os.environ['OMP_NUM_THREADS'] = str(num_threads)
 os.environ['OPENBLAS_NUM_THREADS'] = str(num_threads)
 print('num_threads =', num_threads, end='\n\n')
@@ -14,7 +14,7 @@ print('num_threads =', num_threads, end='\n\n')
 import argparse
 parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument('--atom', type=str, required=True)
-parser.add_argument('-wl', '--wann2lat',  type=int, nargs='+', help='Wann2Lat: <n(0 or int)> <show_tR=0/1>')
+parser.add_argument('-wl', '--wann2lat', type=int, nargs='+', help='Wann2Lat: <n(0/int)> [show_sym=0/1] [show_tR=0/1]')
 parser.add_argument('-gb', '--genband',  type=str, nargs='+', help='GenBand: <path_lat> <Nk>')
 parser.add_argument('-sb', '--showband', type=str, nargs='+', help='ShowBand: <path_band(sep=:)>')
 args = parser.parse_args()                                                                     
@@ -22,12 +22,14 @@ args = parser.parse_args()
 import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 28})
 
+import ase
+from ase.spacegroup import get_spacegroup
+
 import re
 import ctypes
 import cycler
 import numpy as np
 import pandas as pd
-
 Dim = 3
 print('Dim =', Dim, end='\n\n')
 
@@ -53,7 +55,7 @@ def ReadInput(atom):
             elif read_line: A.append(line.split())
             elif re.search('begin unit_cell_cart', line): read_line = 1
         A = np.array(A).astype('d')
-        A /= np.max(A)
+        A /= A[A > 0][0]
 
         read_line = 0
         for line in f:
@@ -62,10 +64,7 @@ def ReadInput(atom):
                 if re.search(atom, line): pos0.append(line.split()[1:])
             elif re.search('begin atoms_frac', line): read_line = 1
         pos0 = np.array(pos0).astype('d')
-
-    pos = np.zeros(pos0.shape)
-    for i, p in enumerate(pos0):
-        for j in range(Dim): pos[i] = np.add(pos[i], p[j]*A[j])
+    pos = np.array([np.sum([p[i]*A[i] for i in range(Dim)], axis=0) for p in pos0])
 
     return path, pos, A 
 
@@ -74,7 +73,7 @@ print('path =', *path, sep='\n')
 print('pos =', *pos, sep='\n')
 print('A =', *A, sep='\n', end='\n\n')
 
-def Wann2Lat(n, show_tR=0):
+def Wann2Lat(n, show_sym=0, show_tR=0):
     pat_site = '[-]?\d+\s+'
     pat_obt  = '[-]?\d+\s+'
     pat_t    = '[-]?\d+[.]\d+\s+'
@@ -83,34 +82,58 @@ def Wann2Lat(n, show_tR=0):
     with open('wannier90_hr.dat', 'r') as f:
         for line in f: 
             if re.search(pat, line): break
-        df = pd.read_csv(f, sep='\s+', names=lat_columns).astype(lat_dtypes)
+        df = pd.read_csv(f, sep='\s+', names=lat_cols).astype(lat_dtypes)
+        Nb = np.max(df['p'])
 
     if n:
-        #df['p_pos'], df['q_pos'] = (df['p']-1) // Nc, (df['q']-1) // Nc
-        R = []
-        for _, d in df.iterrows():
-            #r = np.linalg.norm(d['i']*A[0] + d['j']*A[1] + d['k']*A[2] + pos[int(d['q_pos'])] - pos[int(d['p_pos'])])
-            r = np.linalg.norm(d['i']*A[0] + d['j']*A[1] + d['k']*A[2])
-            R.append(r)
-        df['R'] = R
+        R = np.array([np.sum([d[i]*A[i] for i in range(Dim)], axis=0) for d in df[['i', 'j', 'k']].to_numpy()])
+        R_norm = np.linalg.norm(R, axis=1)
+        df['R_norm'] = R_norm
 
-        R = np.unique(np.round(R, decimals=6))[:n+1]
-        df = df[df['R'] < R[n]]
+        R_norm_uq = np.unique(np.round(R_norm, decimals=6))[:n+1]
+        R = R[R_norm < R_norm_uq[n]]; R_uq = np.unique(R, axis=0)
+        df = df[df['R_norm'] < R_norm_uq[n]]
+
+    if show_sym:
+        poscar_atom = 'POSCAR_%s' % args.atom
+        with open('POSCAR', 'r') as f, open(poscar_atom, 'w') as f_atom:
+            for _ in range(2): f_atom.write(f.readline())
+            for _ in range(Dim): f.readline()
+
+            for i in range(A.shape[0]):
+                for j in range(A.shape[1]): f_atom.write('%23.16f' % A[i][j])
+                f_atom.write('\n')
+
+            atom_idx = f.readline().split().index(args.atom);     f_atom.write('%s\n' % args.atom)
+            atom_num = np.array(f.readline().split(), dtype='i'); f_atom.write('%d\n' % atom_num[atom_idx])
+
+            f_atom.write(f.readline())
+            for _ in range(np.sum(atom_num[:atom_idx])): f.readline()
+            for _ in range(atom_num[atom_idx]): f_atom.write(f.readline())
+
+        sg = get_spacegroup(ase.io.read(poscar_atom))
+        for r in R_uq:
+            sites, _ = sg.equivalent_sites(r)
+            sites = np.array([np.sum([s[i]*A[i] for i in range(Dim)], axis=0) for s in sites])
+            print(r)
+            print(sites, end='\n\n')
 
     if show_tR:
         df['t'] = np.sqrt(df['t_real']**2 + df['t_imag']**2)
 
-        R1 = R[1]
-        t1_max = np.max(df[np.abs(df['R']-R1) < 1e-6]['t'])
+        R1_norm = R_norm_uq[1]
+        t1_max = np.max(df[np.abs(df['R_norm']-R1_norm) < 1e-6]['t'])
+        print('R1_norm =', R1_norm)
+        print('t1_max =', t1_max, end='\n\n')
 
-        print('%6s%16s%16s' % ('n', 'R/R1', 't_max/t1_max'))
-        for i in range(1, len(R)):
-            t_max = np.max(df[np.abs(df['R']-R[i]) < 1e-6]['t'])
-            print('%6d%16.6f%16.6f' % (i, R[i]/R1, t_max/t1_max))
+        print('%6s%16s%16s' % ('n', 'R_norm/R1_norm', 't_max/t1_max'))
+        for i in range(1, len(R_norm_uq)):
+            t_max = np.max(df[np.abs(df['R_norm']-R_norm_uq[i]) < 1e-6]['t'])
+            print('%6d%16.6f%16.6f' % (i, R_norm_uq[i]/R1_norm, t_max/t1_max))
         print()
 
     fn = 'lattice_n%d.txt' % n
-    np.savetxt(fn, df[lat_columns], fmt=['%5d', '%5d', '%5d', '%5d', '%5d', '%12.6f', '%12.6f'])
+    np.savetxt(fn, df[lat_cols], fmt=['%5d', '%5d', '%5d', '%5d', '%5d', '%12.6f', '%12.6f'])
     print('File saved at %s' % fn)
 
 def GenK(Nk):
@@ -145,7 +168,7 @@ def GenBand(path_lat, Nk):
     Nk = int(Nk)
     print('path_lat =', path_lat, '\nNk =', Nk, end='\n\n')
 
-    df = pd.read_csv(path_lat, sep='\s+', names=lat_columns).astype(lat_dtypes)
+    df = pd.read_csv(path_lat, sep='\s+', names=lat_cols).astype(lat_dtypes)
     Nb = int(np.max(df['p']))
     k, k_label, k_point = GenK(Nk)
 
@@ -160,7 +183,7 @@ def GenBand(path_lat, Nk):
     band = np.reshape(np.ctypeslib.as_array(band_c), (Nk, Nb))
 
     os.makedirs(band_path, exist_ok=True)
-    fn = '%s/band_%s_Nk%d.dat' % (band_path, re.sub('.txt', '', re.sub('lattice_', '', path_lat)), Nk)
+    fn = '%s/band_%s_Nk%d.txt' % (band_path, re.sub('.txt', '', re.sub('lattice_', '', path_lat)), Nk)
     np.savetxt(fn, band)
     print('File saved at %s' % fn)
 
@@ -195,7 +218,7 @@ def ShowBand(path_band):
     ax.set_xticks(k_point, labels=k_label)
     ax.set_ylabel(r'$E$')
 
-    fn = '%s/band_' % band_path + '_'.join(['n%d' % ReSubInt('n', p) for p in path_band]) + '_Nk%d.png' % Nk
+    fn = '%s/band_' % band_path + '_'.join([re.sub('_Nk.*', '', re.sub('%s/band_' % band_path, '', p)) for p in path_band]) + '_Nk%d.png' % Nk
     fig.savefig(fn)
     print('Figure saved at %s' % fn)
     plt.show()
